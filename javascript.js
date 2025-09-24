16fa496690b15a5c683cbd422d2865f05ddd35a6
Daniel <oruamtestedeconta@gmail.com>
2025-09-24 17:13:20 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function mostrarTexto() {
+            document.getElementById("texto").innerText = "Professor deixa a gente ir para a quadra por favor🙏!";
+            document.getElementById("botaoInicial").style.display = "none";
+            document.getElementById("botoesExtras").style.display = "block";
+        }
+
+        function respostaSim() {
+            document.getElementById("texto").innerText = "Oba! Vamos jogar bola!⚽⚽⚽";
+            document.getElementById("não").style.display = "none";
+            document.getElementById("sim").style.display = "none";
+            document.getElementById("imagemFutebol").style.display = "none";
+            document.getElementById("videoNey").style.display = "block";
+        }
+
+        function respostaNao() {
+            document.getElementById("texto").innerText = "Noooo la lição nooo😭😭😭";
+            document.getElementById("não").style.display = "none";
+            document.getElementById("sim").style.display = "none";
+            document.getElementById("imagemFutebol").style.display = "none";
+            document.getElementById("videoPolicia").style.display = "block";
+        }
